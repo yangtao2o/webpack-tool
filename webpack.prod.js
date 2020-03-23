97dde508096027d4a9f6b1f1f0ad617020452c5b
yangtao <istaotao@aliyun.com>
2020-03-23 11:05:29 +0800

    fix: split config.js file to dev and prod

---
+++ webpack.prod.js
@@ -0,0 +1,66 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  mode: "production",
+  entry: "./main.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "./dist")
+  },
+  devServer: {
+    contentBase: "./dist"
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "index.html", // 配置输出文件名和路径
+      template: "assets/index.html" // 配置文件模板
+    }),
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: "/public/path/to/"
+            }
+          },
+          "css-loader"
+        ]
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              // name: 'dirname/[contenthash].[ext]',
+              name: "[path][name].[ext]?[contenthash]"
+            }
+          }
+        ]
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+            plugins: ["@babel/plugin-transform-runtime"]
+          }
+        }
+      }
+    ]
+  }
+};
