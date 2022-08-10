06761dd6f25aacd4d53447a3ec414c2ddb14f9fa
yangtao <istaotao@aliyun.com>
2022-08-10 11:19:28 +0800

    feat: 借助Less\Sass\Stylus、PostCSS 等构建现代 CSS 工程环境

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import _ from 'lodash'
 import { say } from './say.ts'
 
+import './index.css'
+import './main.less'
+import './main.scss'
+import './main.styl'
+
 function component() {
   const element = document.createElement('div')
+  element.className = 'main-page'
 
   // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
   element.innerHTML = _.join(['Hello', 'webpack'], ' ')
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,15 @@
 const path = require('path')
 const ESLintPlugin = require('eslint-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+
+console.log('env', process.env.NODE_ENV)
+
+const getStyleLoader = () => {
+  return process.env.NODE_ENV === 'development'
+    ? 'style-loader'
+    : MiniCssExtractPlugin.loader
+}
 
 module.exports = {
   entry: './src/index',
@@ -26,9 +36,35 @@ module.exports = {
         test: /\.ts$/,
         use: 'ts-loader',
       },
+      {
+        test: /\.css$/i,
+        use: [
+          getStyleLoader(),
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1, //当css-loader遇到@import语法时
+            },
+          },
+          'postcss-loader',
+        ],
+      },
+      {
+        test: /\.less$/,
+        use: [getStyleLoader(), 'css-loader', 'postcss-loader', 'less-loader'],
+      },
+      {
+        test: /\.s(a|c)ss$/,
+        use: [getStyleLoader(), 'css-loader', 'postcss-loader', 'sass-loader'],
+      },
+      {
+        test: /\.styl$/,
+        use: [getStyleLoader(), 'css-loader', 'stylus-loader'],
+      },
     ],
   },
   resolve: {
     extensions: ['.ts', '.js'],
   },
+  plugins: [new MiniCssExtractPlugin(), new HTMLWebpackPlugin()],
 }
