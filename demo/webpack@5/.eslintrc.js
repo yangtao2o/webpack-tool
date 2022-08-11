19f57f4d6d581f4f61b1142a8686fd445f6d6cb5
yangtao <istaotao@aliyun.com>
2022-08-11 13:47:29 +0800

    chore: eslint+prettier config

---
+++ demo/webpack@5/.eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: ['plugin:prettier/recommended'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+}
